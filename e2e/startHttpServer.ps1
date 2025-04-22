de798e6e93f7396e17683f966a1e99946567bf9c
AleksaBajat <aleksabajat15@gmail.com>
2025-04-22 13:28:07 +0200

    fix: flaky blackhole and dns tests - set integration test port to 8084 instead of 8085 so we don't get overlaps if the system already has extension host installed. - few improvements on startHttpServer script.

--- e2e/startHttpServer.ps1
+++ e2e/startHttpServer.ps1
@@ -5,34 +5,37 @@ $l.Prefixes.Add("http://{{.Ip}}:{{nextPort .Port}}/");
 $l.Start();
 Write-Host "Listening on http://{{.Ip}}:{{.Port}}/";
 while ($l.IsListening) {
-  $con = $l.GetContext();
-  $req = $con.Request;
-  $res = $con.Response;
-  if ($req.Url.Query -match "url=([^&]+)") {
-    try {
-      $targetUrl = [System.Web.HttpUtility]::UrlDecode($matches[1]);
-      if (-not $targetUrl.StartsWith("http")) {
-        $targetUrl = "http://" + $targetUrl
-      };
+    $con = $l.GetContext();
+    $req = $con.Request;
+    $res = $con.Response;
 
-      # Use Invoke-WebRequest instead of WebClient
-      $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
-      $response = Invoke-WebRequest -Uri $targetUrl -UserAgent $userAgent -UseBasicParsing;
+    if ($req.Url.Query -match "url=([^&]+)") {
+        try {
+            $targetUrl = [System.Web.HttpUtility]::UrlDecode($matches[1]);
+            if (-not $targetUrl.StartsWith("http")) {
+                $targetUrl = "http://" + $targetUrl;
+            }
 
-      # Handle the content based on the content type
-      $contentBytes = [System.Text.Encoding]::UTF8.GetBytes($response.Content);
-      if ($response.Headers.ContainsKey("Content-Type")) {
-        $res.ContentType = $response.Headers["Content-Type"];
-      }
+            $userAgent = "Mozilla/5.0 ... Chrome";
+            $response = Invoke-WebRequest -Uri $targetUrl -UserAgent $userAgent -UseBasicParsing;
 
-      $res.OutputStream.Write($contentBytes, 0, $contentBytes.Length);
-    } catch {
-      $err = [System.Text.Encoding]::UTF8.GetBytes("Proxy error: $_");
-      $res.StatusCode = 500;
-      $res.OutputStream.Write($err, 0, $err.Length);
+            $contentBytes = [System.Text.Encoding]::UTF8.GetBytes($response.Content);
+
+            if ($response.Headers["Content-Type"]) {
+                $res.ContentType = $response.Headers["Content-Type"];
+            }
+
+            $res.ContentLength64 = $contentBytes.Length
+            $res.OutputStream.Write($contentBytes, 0, $contentBytes.Length);
+        } catch {
+            $err = [System.Text.Encoding]::UTF8.GetBytes("Proxy error: $_");
+            $res.StatusCode = 500;
+            $res.OutputStream.Write($err, 0, $err.Length);
+        }
+    } else {
+        $c = [System.Text.Encoding]::UTF8.GetBytes("<html><body>Hello, Windows HTTP Server</body></html>");
+        $res.OutputStream.Write($c, 0, $c.Length);
     }
-  } else {
-    $c = [System.Text.Encoding]::UTF8.GetBytes("<html><body>Hello, Windows HTTP Server</body></html>");
-    $res.OutputStream.Write($c, 0, $c.Length);
-  };
+    $res.OutputStream.Flush()
+    $res.Close()
 }
