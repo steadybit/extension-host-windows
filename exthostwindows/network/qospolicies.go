47b2eacb98aba22652a206c3bd9aea30e7f65cea
Norbert Schneider <norbert@steadybit.com>
2025-05-05 09:49:42 +0200

    feat: execute bandwidth command in system context

--- exthostwindows/network/qospolicies.go
+++ exthostwindows/network/qospolicies.go
@@ -5,6 +5,7 @@ package network
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/steadybit/extension-host-windows/exthostwindows/utils"
@@ -26,29 +27,23 @@ func logCurrentQoSRules(ctx context.Context, when string) {
 }
 
 func listSteadybitQosPolicies(ctx context.Context) ([]string, error) {
-	listCommand := "Get-NetQosPolicy | Where-Object { $_.Name -like \"" + qosPolicyPrefix + "*\" }"
-	result, err := utils.ExecutePowershellCommand(ctx, []string{listCommand}, utils.PSRun)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list QoS policies: %w", err)
-	}
-	var policies []string
-	blocks := strings.Split(result, "\r\n\r\n")
-	for _, block := range blocks {
-		if block != "" {
-			policies = append(policies, block)
-		}
-	}
-	return policies, nil
+	return listQosPolicies(ctx, "Get-NetQosPolicy | Where-Object { $_.Name -like \""+qosPolicyPrefix+"*\" }", "\r\n\r\n")
 }
 
 func listSteadybitQosPolicyNames(ctx context.Context) ([]string, error) {
-	listCommand := "Get-NetQosPolicy | Where-Object { $_.Name -like \"" + qosPolicyPrefix + "*\" } | Select-Object -ExpandProperty Name"
-	result, err := utils.ExecutePowershellCommand(ctx, []string{listCommand}, utils.PSRun)
+	return listQosPolicies(ctx, "Get-NetQosPolicy | Where-Object { $_.Name -like \""+qosPolicyPrefix+"*\" } | Select-Object -ExpandProperty Name", "\r\n")
+}
+
+func listQosPolicies(ctx context.Context, command string, separator string) ([]string, error) {
+	result, err := utils.ExecutePowershellCommand(ctx, []string{command}, utils.PSRun)
+
+	log.Info().Msgf("result: %s", result)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to list QoS policies: %w", err)
 	}
 	var policies []string
-	blocks := strings.Split(result, "\r\n")
+	blocks := strings.Split(result, separator)
 	for _, block := range blocks {
 		if block != "" {
 			policies = append(policies, block)
@@ -65,21 +60,20 @@ func removeSteadybitQosPolicies(ctx context.Context) error {
 	if len(policies) == 0 {
 		return nil
 	}
-
 	log.Error().Strs("policies", policies).Msg("Found leftover QoS policies, removing them")
 	return removeQoSPolicies(ctx, policies)
 }
 
 func removeQoSPolicies(ctx context.Context, policies []string) error {
-	var errors []string
+	var errs error
 	for _, policy := range policies {
-		removeCommand := fmt.Sprintf("Remove-NetQosPolicy -Name %s -Confirm:$false", policy)
-		if _, err := utils.ExecutePowershellCommand(ctx, []string{removeCommand}, utils.PSRun); err != nil {
-			errors = append(errors, err.Error())
+		removeCommand := fmt.Sprintf("Remove-NetQosPolicy -Name %s -Confirm:`$false", policy)
+		if _, err := utils.ExecutePowershellCommand(ctx, utils.BuildSystemCommandFor(removeCommand), utils.PSRun); err != nil {
+			errs = errors.Join(errs, err)
 		}
 	}
-	if len(errors) > 0 {
-		return fmt.Errorf("failed to remove QoS policies: %s", strings.Join(errors, "; "))
+	if errs != nil {
+		return fmt.Errorf("failed to remove QoS policies: %w", errs)
 	}
 	return nil
 }
